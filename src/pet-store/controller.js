171419a3547169256f24186fb3fd324caae976d0
Mohamed Akl <mohamed.akl@blink22.com>
2022-10-29 16:36:14 +0200

    added comments

--- src/pet-store/controller.js
+++ src/pet-store/controller.js
@@ -1,6 +1,7 @@
 const { addBid, addPet: addPetToModel, getBids } = require("./model");
 const { petExists, isOwner, getBidWinners } = require("./service");
 
+//adds user bid only if the pet exists.
 const addUserBid = async ({ name, petId, amount }) => {
   if (await petExists(petId)) return await addBid(name, petId, amount);
   else {
@@ -15,6 +16,7 @@ const addPet = async ({ owner, petId }) => {
   return await addPetToModel(owner, petId);
 };
 
+//list all bids on specific pet if the user is the owner of the pet
 const listBids = async ({ email, petId }) => {
   if (await isOwner(email, petId)) {
     return await getBids(petId);
@@ -23,11 +25,13 @@ const listBids = async ({ email, petId }) => {
   }
 };
 
+//gets auction result if there are bids or the pet exists, otherwise it returns "no Winners"
 const getAuctionResult = async (petId) => {
   const bids = await getBids(petId);
   return calculateWinners(bids);
 };
 
+//gets bids from the request and runs the script to calculate the winner
 const getAuctionWinners = (bids) => calculateWinners(bids);
 
 const calculateWinners = (bids) => {
